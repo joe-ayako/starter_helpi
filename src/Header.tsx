802c807531905e67fc5aee884b61c2d4ad8224a1
Axel <axelarl@udel.edu>
2024-11-08 13:36:18 -0500

    Added title to the main page

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <header style={{ backgroundColor: 'blue', color: 'white', padding: '1rem', textAlign: 'center' }}>
       <a href="/">
+      <h1 style={{ fontSize: '48px', color: '#61dafb' }}>The Very Cool Career Quiz</h1>
         <button>Go to Home</button>
       </a>
     </header>
