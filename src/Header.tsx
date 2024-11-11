83b13c7365835a4dd16b08372aea027dcdf0c298
Axel <axelarl@udel.edu>
2024-11-11 13:33:34 -0500

    Changed background color from blue to Ochre

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header style={{ backgroundColor: 'blue', color: 'white', padding: '1rem', textAlign: 'center' }}>
+    <header style={{ backgroundColor: '#bd7415', color: 'white', padding: '1rem', textAlign: 'center' }}>
       <a href="/">
+        <h1> Career Cuisine</h1>
         <button>Go to Home</button>
       </a>
     </header>
